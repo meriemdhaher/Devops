e553e67408c2a0b16f3af0d8662471dc6156c646
meriemdhaher <128425485+meriemdhaher@users.noreply.github.com>
2024-11-04 11:00:31 +0100

    commit

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -2,9 +2,13 @@ package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;;
+import tn.esprit.spring.services.CourseServicesImpl;
 
-@SpringBootTest
+@SpringBootTest(classes = CourseServicesImpl.class)
 class GestionStationSkiApplicationTests {
+	@MockBean
+	private CourseServicesImpl courseServices;
 
 	@Test
 	void contextLoads() {
