87c7d7e54de81ab1d21665ec4bcd1a1082d3d952
beddouihech ghassen <big97394@gmail.com>
2024-11-06 05:19:24 +0100

    c

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -2,10 +2,15 @@ package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import tn.esprit.spring.repositories.ISubscriptionRepository;
+import tn.esprit.spring.services.SubscriptionServicesImpl;
 
-@SpringBootTest
+
+@SpringBootTest(classes = SubscriptionServicesImpl.class)
 class GestionStationSkiApplicationTests {
-
+	@MockBean
+	private SubscriptionServicesImpl subscriptionServices;
 	@Test
 	void contextLoads() {
 	}
